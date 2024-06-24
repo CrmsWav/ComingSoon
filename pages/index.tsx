d1f94586411fc8b10fbd5313703aeda8c18ceac0
CrmsWav <marcustritant@gmail.com>
2024-06-24 22:47:58 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
 import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Coming Soon - Clean Coming Soon Page (Tailwind CSS, NextJs & HTML5) created by themeptation.net">
-    <img
+    <Image
       src="/images/xera.svg"
       alt="Themeptation "
+      width={500} // ajoutez la largeur de l'image ici
+      height={500} // ajoutez la hauteur de l'image ici
       className="absolute h-96 -top-20 -right-16 lg:right-5 lg:top-10 animate-blob"
     />
-    <img
+    <Image
       src="/images/shapes.svg"
       alt="hero"
+      width={1000} // ajoutez la largeur de l'image ici
+      height={500} // ajoutez la hauteur de l'image ici
       className="absolute w-full left-24 bottom-24 animate-blob2"
     />
     <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900">
@@ -38,8 +43,8 @@ const IndexPage = () => (
     <div className="absolute bottom-5">
       <ul className="flex space-x-3">
         <li>
-          <Link href="https://themeptation.net">
-            <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+          <Link href="https://themeptation.net" passHref>
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
               <svg
                 className="w-6 h-6"
                 fill="currentColor"
@@ -52,12 +57,12 @@ const IndexPage = () => (
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </a>
+            </div>
           </Link>
         </li>
         <li>
-          <Link href="https://themeptation.net">
-            <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+          <Link href="https://themeptation.net" passHref>
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
               <svg
                 className="w-6 h-6"
                 fill="currentColor"
@@ -66,12 +71,12 @@ const IndexPage = () => (
               >
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
               </svg>
-            </a>
+            </div>
           </Link>
         </li>
         <li>
-          <Link href="https://themeptation.net">
-            <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+          <Link href="https://themeptation.net" passHref>
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
               <svg
                 className="w-6 h-6"
                 fill="currentColor"
@@ -84,12 +89,12 @@ const IndexPage = () => (
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </a>
+            </div>
           </Link>
         </li>
         <li>
-          <Link href="https://themeptation.net">
-            <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+          <Link href="https://themeptation.net" passHref>
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
               <svg
                 className="w-6 h-6"
                 fill="currentColor"
@@ -102,7 +107,7 @@ const IndexPage = () => (
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </a>
+            </div>
           </Link>
         </li>
       </ul>
